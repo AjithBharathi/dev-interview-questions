0d00440faacc0794aabff7ba9f44a8631a55c917
abgunasekaran <ajithbharathi11@gmail.com>
2021-06-07 21:42:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Html } from './data/html';
+import { Css } from './data/css';
+import { Javascript } from './data/javascript';
+import { Angular } from './data/angular';
+
+@Pipe({ name: 'filter' })
+export class FilterPipe implements PipeTransform {
+  transform(items: any[], searchText: string): any[] {
+    if (!items) return [];
+    if (!searchText) return items;
+
+    let returnItems: any = items.filter(item => {
+      return Object.keys(item).some(key => {
+        return String(item[key]).toLowerCase().includes(searchText.toLowerCase());
+      });
+    });
+    return returnItems;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [FilterPipe]
 })
-export class AppComponent {
-  title = 'dev-interview';
+export class AppComponent implements OnInit {
+  title = "Interview Questions";
+  searchInput: string = "";
+  showTitle: boolean = true;
+  options = [
+    { label: 'HTML', value: 'html' },
+    { label: 'CSS', value: 'css' },
+    { label: 'Javascript', value: 'js' },
+    { label: 'Angular', value: 'angular' },
+  ];
+  questions: any = [];
+  questionsCopy: any = [];
+
+  scrollTop = 0;
+  hideNav = false;
+
+  constructor(private filterPipe: FilterPipe, private element: ElementRef) {
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (window.innerWidth <= 768) {
+      this.showTitle = false;
+    }
+    else {
+      this.showTitle = true;
+    }
+  }
+
+  onScroll(event: any) {
+    this.hideNav = this.scrollTop < event.target.scrollTop;
+    this.scrollTop = event.target.scrollTop;
+  }
+
+  ngOnInit() {
+    if (window.innerWidth <= 768) {
+      this.showTitle = false;
+    }
+    else {
+      this.showTitle = true;
+    }
+
+    this.questions = Html;
+    this.questionsCopy = Html;
+  }
+
+  optionChange(val: string) {
+    switch (val) {
+      case 'html':
+        this.questions = Html;
+        this.questionsCopy = Html;
+        break;
+      case 'css':
+        this.questions = Css;
+        this.questionsCopy = Css;
+        break;
+      case 'js':
+        this.questions = Javascript;
+        this.questionsCopy = Javascript;
+        break;
+      case 'angular':
+        this.questions = Angular;
+        this.questionsCopy = Angular;
+        break;
+      default:
+        this.questions = Html
+        this.questionsCopy = Html
+    }
+    let el = this.element.nativeElement.querySelector('#drawer_content');
+    el.scrollTo(0, 0);
+  }
+
+  onUpdateFilter() {
+    this.questions = this.filterPipe.transform(this.questionsCopy, this.searchInput);
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, FilterPipe } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
